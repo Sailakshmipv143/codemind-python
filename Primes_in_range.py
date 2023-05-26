8a025a672a9654d6747ebb08796d5a34c078f628
Sailakshmipv143 <99871125+Sailakshmipv143@users.noreply.github.com>
2023-05-26 10:47:09 +0530

    def is_prime(n):     if n<2:         return False     if n==2:         return True     if n%2==0:         return False     for  i in range(3,int(n**0.5)+1,2):         if n%i==0:            return False     return True m=int(input()) n=int(input()) count=0 for i in range(m,n+1):     if is_prime(i):         count+=1 print(count)

---
+++ Primes_in_range.py
@@ -0,0 +1,18 @@
+def is_prime(n):
+    if n<2:
+        return False
+    if n==2:
+        return True
+    if n%2==0:
+        return False
+    for  i in range(3,int(n**0.5)+1,2):
+        if n%i==0:
+           return False
+    return True
+m=int(input())
+n=int(input())
+count=0
+for i in range(m,n+1):
+    if is_prime(i):
+        count+=1
+print(count)
