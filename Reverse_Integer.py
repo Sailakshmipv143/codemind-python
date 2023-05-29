b80fb194c0c78e1990a2e228b4dd79f04784266e
Sailakshmipv143 <99871125+Sailakshmipv143@users.noreply.github.com>
2023-05-29 21:15:10 +0530

    n=int(input()) s=0 k=0 if n<0:     k=1     n=abs(n) while(n!=0):     r=n%10     s=s*10+r     n=n//10 if k==1:     print("-"+str(s))     exit() print(s)

--- Reverse_Integer.py
+++ Reverse_Integer.py
@@ -2,14 +2,13 @@ n=int(input())
 s=0
 k=0
 if n<0:
-   k=1
-   n=abs(n)
+    k=1
+    n=abs(n)
 while(n!=0):
-  r=n%10
-  s=s*10+r
-  n=n//10
+    r=n%10
+    s=s*10+r
+    n=n//10
 if k==1:
-  print("-"+str(s))
-  exit()
+    print("-"+str(s))
+    exit()
 print(s)
-
