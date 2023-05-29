56c2287c14d058b3159b100d89662478c28ac5f4
Sailakshmipv143 <99871125+Sailakshmipv143@users.noreply.github.com>
2023-05-29 21:45:25 +0530

    n=int(input()) l=[] while(n>0):     r=n%10     l.append(r)     n=n//10 s=l[0] for i in range(len(l)):     if s<l[i]:         s=l[i] print(s)

--- Largest_Digit.py
+++ Largest_Digit.py
@@ -1,11 +1,11 @@
 n=int(input())
 l=[]
 while(n>0):
-   r=n%10
-   l.append(r)
-   n=n//10
+    r=n%10
+    l.append(r)
+    n=n//10
 s=l[0]
 for i in range(len(l)):
-  if s<l[i]:
-    s=l[i]
+    if s<l[i]:
+        s=l[i]
 print(s)
